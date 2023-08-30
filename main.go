6c6ceff8327cc357e111d07180e54bca05ea9f20
kn1ko1 <niquaye@gmail.com>
2023-08-30 17:46:37 +0100

    comments deleted

--- main.go
+++ main.go
@@ -5,21 +5,6 @@ import (
 	"net/http"
 )
 
-// github client ID
-// 4764bca26a3fa9ab0e32
-
-//github secret
-// 4b97506ea58726f63c065ce8ed60b35c1fcbf532
-
-// Client ID
-// 623060904480-iudd85gmtab7k4sr1utdo92e5lpfsuqd.apps.googleusercontent.com
-
-// Client secret
-// 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
-
-// Creation date
-// 28 June 2023 at 14:21:45 GMT+1
-
 func main() {
 
 	http.HandleFunc("/", f.ServePage)
